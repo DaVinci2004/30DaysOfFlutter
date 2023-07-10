19191ebcb0a42f01b9dda8c2405eb0a727e69247
Suraj Sen <Suraj11082004@gmail.com>
2023-07-11 00:29:53 +0530

    day 4 competed

--- flutter_application_1/lib/pages/login_page.dart
+++ flutter_application_1/lib/pages/login_page.dart
@@ -4,15 +4,62 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child : Center(
-          child: Text("Login Page",style: TextStyle(
-            fontSize: 20,
-            color: Color.fromARGB(255, 25, 35, 225),
-            fontWeight: FontWeight.bold,
-            ),
+    return Material(
+      color: Colors.white,
+      child : Column(
+        children: [
+          Image.asset("assets/images/undraw_secure_login_pdn4.png",fit: BoxFit.cover
           ),
-        ),
+          SizedBox(
+            height: 20,
+            
+          ),
+           Text("Welcome", 
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+          ),
+          SizedBox(
+            height: 20,
+            
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+            child: Column(
+              children: [
+                TextFormField(
+              decoration: InputDecoration(
+                hintText: "Enter Username",
+                labelText: "Username",
+              ),
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Enter Password",
+                labelText: "Password",
+              ),
+            ),
+            SizedBox(
+            height: 20,
+            
+          ),
+          ElevatedButton(
+            onPressed: (){
+              print("Hi")
+            }, 
+            child: Text("Login"),
+            style: TextButton.styleFrom().
+              
+
+            )
+
+              ],
+            ),
+          )
+        ],
+        )
     );
   }
 }
